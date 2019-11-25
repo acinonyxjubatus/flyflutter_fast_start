d3659338f0b95e055a6040479efa4eb2b69804da
acinonyx <andreyaleev.mail@gmail.com>
2019-11-25 16:37:38 +0300

    CounterWidget as StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,77 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'StatelessWidget sample',
+      title: 'Counter',
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
       home: Scaffold(
         backgroundColor: Colors.amber[300],
         appBar: AppBar(
-          title: Text('Labels'),
+          title: Text('Counter on Flutter'),
         ),
         body: Center(
-          child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                HintLabel('custom label 1'),
-                SizedBox(height: 8.0),
-                Text('text widget'),
-                SizedBox(height: 8.0),
-                HintLabel('custom label 2')
-              ]),
-        ),
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  HintLabel('tap - to decrement'),
+                  SizedBox(height: 8.0),
+                  CounterWidget(),
+                  SizedBox(height: 8.0),
+                  HintLabel('tap + to increment')
+                ])),
       ),
     );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _count = 42;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          color: Colors.amber[600],
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(
+                onPressed: () {
+                  _decrement();
+                },
+                icon: Icon(Icons.remove)),
+            Text('$_count', style: TextStyle(fontSize: 20.0)),
+            IconButton(
+                onPressed: () {
+                  _increment();
+                },
+                icon: Icon(Icons.add)),
+          ],
+        ));
+  }
+
+  void _increment() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      _count--;
+    });
   }
 }
 
@@ -45,7 +91,8 @@ class HintLabel extends StatelessWidget {
       decoration: BoxDecoration(color: Colors.amber[200]),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Text(text, style: TextStyle(color: Colors.grey[700])),
+        child: Text(text,
+            style: TextStyle(color: Colors.grey[700])),
       ),
     );
   }
