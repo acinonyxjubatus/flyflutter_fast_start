6f3e679acd8a490af6dfa6a9a0fdefe1f71457ff
acinonyx <andreyaleev.mail@gmail.com>
2019-11-25 16:29:13 +0300

    custom StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,45 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'StatelessWidget sample',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.amber[300],
         appBar: AppBar(
-          title: Text('This is Flutter'),
+          title: Text('Labels'),
         ),
         body: Center(
-          child: Text('Hello World!',
-              style: TextStyle(
-                fontSize: 42.0, // делаем текст большим
-                fontWeight: FontWeight.bold,  // жирным
-                color: Colors.white, // белым
-              )
-          ),
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                HintLabel('custom label 1'),
+                SizedBox(height: 8.0),
+                Text('text widget'),
+                SizedBox(height: 8.0),
+                HintLabel('custom label 2')
+              ]),
         ),
+      ),
+    );
+  }
+}
+
+class HintLabel extends StatelessWidget {
+  final String text;
+
+  const HintLabel(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(color: Colors.amber[200]),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(text, style: TextStyle(color: Colors.grey[700])),
       ),
     );
   }
